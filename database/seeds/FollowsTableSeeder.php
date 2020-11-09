b856ccde715ea3aedfe4dd1df01000085614ea57
natsumi.k <k.natsumi.boc@gmail.com>
2020-11-10 01:42:39 +0900

    FTS 7

--- database/seeds/FollowsTableSeeder.php
+++ database/seeds/FollowsTableSeeder.php
@@ -12,9 +12,18 @@ class FollowsTableSeeder extends Seeder
     public function run()
     {
         DB::connection('mysql')->table('follows')->insert([
-
-          
-
+          [
+            'user_id' => '1',
+            'follow_id' => '2'
+          ],
+          [
+            'user_id' => '1',
+            'follow_id' => '3'
+          ],
+          [
+            'user_id' => '3',
+            'follow_id' => '1'
+          ],
         ]);
     }
 }
